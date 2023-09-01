ddf58c686fc262e30dea783ffa6d0f15dbc3cb56
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-08-31 19:54:54 -0500

    optimizing the http requests

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -13,11 +15,24 @@ const healthCheckInterval = 1 * time.Minute
 const appsConfigPath = "apps.json"
 const outputFilePath = "output.txt"
 
+var client = &http.Client{
+	Timeout: time.Second * 5,
+}
+
 // App represents an application hosted on a node
 type App struct {
 	Domain         string `json:"domain"`
 	HealthEndpoint string `json:"healthEndpoint"`
 }
+
+// Metrics tracks the different timestamps for an application domain
+type Metrics struct {
+	Last200     time.Time
+	LastRequest time.Time
+	LastFail    time.Time
+}
+
+var domainMetrics sync.Map
 
 func main() {
 	ticker := time.NewTicker(healthCheckInterval)
@@ -34,19 +49,39 @@ func main() {
 func checkHealthAndReport() {
 	apps := loadAppsConfiguration()
 
-	var output string
+	// Using strings.Builder for efficient string concatenation
+	var builder strings.Builder
+
 	for host, domains := range apps {
 		for _, domainConfig := range domains {
 			status := "DOWN"
-			resp, err := http.Get("http://" + host + domainConfig.HealthEndpoint)
-			if err == nil && resp.StatusCode == 200 {
-				status = "UP"
+			currentTime := time.Now()
+			resp, err := client.Get("http://" + host + domainConfig.HealthEndpoint)
+
+			// Initialize metrics if not existent and also load them in one step
+			metrics, _ := domainMetrics.LoadOrStore(domainConfig.Domain, &Metrics{})
+			metricsPtr := metrics.(*Metrics)
+			metricsPtr.LastRequest = currentTime
+
+			if err == nil {
+				defer resp.Body.Close()
+				if resp.StatusCode == 200 {
+					status = "UP"
+					metricsPtr.Last200 = currentTime
+				} else {
+					metricsPtr.LastFail = currentTime
+				}
+			} else {
+				metricsPtr.LastFail = currentTime
 			}
-			output += fmt.Sprintf("%s: %s\n", domainConfig.Domain, status)
+
+			// Writing the output to the file
+			builder.WriteString(fmt.Sprintf("%s: %s | Last 200: %s | Last Request: %s | Last Fail: %s\n",
+				domainConfig.Domain, status, metricsPtr.Last200, metricsPtr.LastRequest, metricsPtr.LastFail))
 		}
 	}
 
-	writeToFile(outputFilePath, output)
+	writeToFile(outputFilePath, builder.String())
 }
 
 func loadAppsConfiguration() map[string][]App {
